77e69799fec62c65ef6a7b79d2dc5816d860a195
Oshadha Basnayaka <oshadhabasnayaka69@gmail.com>
2023-08-27 21:17:56 +0530

    update

--- Back_End/src/lk/ijse/jsp/pos/servlet/PurchaseOrderServletAPI.java
+++ Back_End/src/lk/ijse/jsp/pos/servlet/PurchaseOrderServletAPI.java
@@ -1,6 +1,19 @@
 package lk.ijse.jsp.pos.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class PurchaseOrderServletAPI extends HttpServlet {
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
