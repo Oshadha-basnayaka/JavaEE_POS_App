6790ca212c95630815564a6160ad677f6e0b7bcb
Oshadha Basnayaka <oshadhabasnayaka69@gmail.com>
2023-08-27 20:12:16 +0530

    customer

--- Back_End/src/lk/ijse/jsp/pos/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/jsp/pos/servlet/CustomerServletAPI.java
@@ -13,7 +13,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@WebServlet
+@WebServlet (urlPatterns = {"/pages/customer"})
 public class CustomerServletAPI extends HttpServlet {
 
 
